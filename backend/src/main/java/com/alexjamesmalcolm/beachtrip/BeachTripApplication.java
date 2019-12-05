8dd7e6b261ecc5f643e629f931ef374d5e49581f
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-12-05 18:09:14 -0500

    Initial commit

---
+++ backend/src/main/java/com/alexjamesmalcolm/beachtrip/BeachTripApplication.java
@@ -0,0 +1,13 @@
+package com.alexjamesmalcolm.beachtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachTripApplication.class, args);
+	}
+
+}
